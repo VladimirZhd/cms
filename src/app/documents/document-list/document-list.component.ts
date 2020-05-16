6db78ee687ebfd42a601f3557bc8802cf61ea87b
VladimirZhd <zhd18001@byui.edu>
2020-05-16 16:23:23 -0600

    up

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document(1, "Document 1", "This is document 1", "https://byui.edu", []),
+    new Document(2, "Document 2", "This is document 2", "https://byui.edu", []),
+    new Document(3, "Document 3", "This is document 3", "https://byui.edu", []),
+    new Document(4, "Document 4", "This is document 4", "https://byui.edu", []),
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
